a74a337fdd1fdaa1d1d1ab9552bcfa9447d8449f
FulmenMinis <erdos.horvath.emese@gmail.com>
2017-12-15 00:45:20 +0100

    Added some error handling

--- todo.py
+++ todo.py
@@ -13,7 +13,11 @@ class Todo(object):
             {"argument": "-r", "description": "Removes a task"},
             {"argument": "-c", "description": "Completes a task"}
         ]
-        if len(sys.argv) == 2 and sys.argv[1] == "-l":
+        if len(sys.argv) == 1:
+            print ("\n Command Line Todo application \n ============================= " + 
+            "\n Command line arguments: \n  -l   Lists all the tasks \n  -a   " + 
+            "Adds a new task \n  -r   Removes an task \n  -c   Completes an task")
+        elif len(sys.argv) == 2 and sys.argv[1] == "-l":
             return self.list_todo()
         elif len(sys.argv) == 3 and sys.argv[1] == "-a":
             item = str(sys.argv[2])
@@ -24,47 +28,60 @@ class Todo(object):
         elif len(sys.argv) == 3 and sys.argv[1] == "-c":
             index = int(sys.argv[2])
             return self.check_item(index)
-        else: print ("\n Command Line Todo application \n ============================= " + 
-            "\n Command line arguments: \n  -l   Lists all the tasks \n  -a   " + 
-            "Adds a new task \n  -r   Removes an task \n  -c   Completes an task")
+        else: 
+            print("Unable to add: no task provided")
     
     def list_todo(self):
-        with open('todo_list.txt') as f:
+        try:
+            file = open("todo_list.txt") 
             lines = [line.rstrip('\n') for line in open('todo_list.txt')] #rstrip
             index_num = 1
-            for i in lines:
-                print(index_num, i)
-                index_num += 1    
-
+            file.close()
+            if len(lines) == 0:
+                print("There are no todos for today :)")
+            else:
+                for i in lines:
+                    print(index_num, i)
+                    index_num += 1    
+        except FileNotFoundError:
+            return 0
+    
     def add_todo(self, item):
-        file = open("todo_list.txt", "a") 
-        file.write(str("\n" + "[ ] " + item)) 
-        file.close()
-        if sys.argv[1] == "-a" and sys.argv[2] == "": # ha egy lista csak 2 elemű és a 2. eleme a akkor nincs érv input mögötte
-            print("Unable to add: no task provided")
+        try:
+            file = open("todo_list.txt", "a")
+            file.write(str("\n" + "[ ] " + item))
+            file.close()
+        except IOError:
+            print("Unable to write file.")
 
     def remove_item(self, index):
-        with open("todo_list.txt","r") as textfile:
-            listed = list(textfile)
-            del listed[index - 1]    
-        with open("todo_list.txt","w") as textfile:
-            for n in listed:
-                textfile.write(n)    
+        try:
+            with open("todo_list.txt","r") as textfile:
+                listed = list(textfile)
+                del listed[index - 1]    
+            with open("todo_list.txt","w") as textfile:
+                for n in listed:
+                    textfile.write(n)    
+        except IOError:
+            print("Unable to write file.")
 
     def check_item(self, index):
-        tasks = open("todo_list.txt", "r")
-        line = tasks.readlines()
-        complete = int(sys.argv[2]) - 1
-        tasks.close()
-        if int(sys.argv[2]) > len(line):
-            return print("Unable to check: index is out of bound")
-        tasks = open("todo_list.txt", "w")
-        for i in range(len(line)):
-            if line[i][0:3] == "[ ]" and i == complete:
-                tasks.write("[x]" + line[i][3:])
-            else:
-                tasks.write(line[i])
-        tasks.close()
+        try:
+            tasks = open("todo_list.txt", "r")
+            line = tasks.readlines()
+            complete = int(sys.argv[2]) - 1
+            tasks.close()
+            if int(sys.argv[2]) > len(line):
+                return print("Unable to check: index is out of bound")
+            tasks = open("todo_list.txt", "w")
+            for i in range(len(line)):
+                if line[i][0:3] == "[ ]" and i == complete:
+                    tasks.write("[x]" + line[i][3:])
+                else:
+                    tasks.write(line[i])
+            tasks.close()
+        except FileNotFoundError:
+            return 0
         
 app = Todo()
 app.controller()
